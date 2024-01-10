d7335f7a6288f1b4786505ff5f650811531f9f3e
netra2060002 <np03cs4s210099@heraldcollege.edu.np>
2024-01-10 17:37:11 +0545

    Uploading quiz app in github.

---
+++ src/Data/QuizData.js
@@ -0,0 +1,32 @@
+export const QuizData = [
+  {
+    question: "which language runs in a web browser?",
+    options: ["java", "C", "Python", "JavaScript"],
+    answer: 4,
+  },
+  {
+    question: "what does CSS stand for?",
+    options: [
+      "Central style Sheets",
+      "Cascading Style Sheets",
+      "Cascading Simple Sheets",
+      "Cars SUVs Sailboats",
+    ],
+    answer: 2,
+  },
+  {
+    question: "What does HTML stand for?",
+    options: [
+      "Hypertext Markup Language",
+      "Hypertext Markdown Language",
+      "Hyperloop Machine Language",
+      "Helicopters Terminals Motorboats Lamborginis",
+    ],
+    answer: 1,
+  },
+  {
+    question: "What year was JavaScript launched?",
+    options: ["1996", "1995", "1994", "none of the above"],
+    answer: 2,
+  },
+];
---
+++ src/components/QuizResult.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const QuizResult = (props) => {
+  return (
+    <>
+      <div className="show-score">
+        Your score:{props.score} <br />
+        totalscore:{props.totalscore}
+      </div>
+      <button id="next-button" onClick={props.tryagain}>
+        try again
+      </button>
+    </>
+  );
+};
+
+export default QuizResult;
